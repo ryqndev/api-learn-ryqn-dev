e962ad5a8ceb4ef119af6ab6c4302bea3c1c85e0
Ryan Yang <ryanqyang@gmail.com>
2020-01-06 04:59:28 -0800

    added full link

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const readAPISchema = () => {
 }
 
 const generateStaticAPI = (APIRoutes) => {
-    let path = './static';
+    let path = '/static';
     buildFolder( APIRoutes, path );
 }
 
@@ -15,12 +15,12 @@ const buildFolder = ( APIRoutes, path ) => {
     if (!APIRoutes.hasOwnProperty('content'))
         return;
     return new Promise(res => {
-        fs.mkdir(path, { recursive: true }, (err) => {
+        fs.mkdir('.' + path, { recursive: true }, (err) => {
             if (err) throw err;
             let paths = APIRoutes.content.map(e => {
                 let nextPath = `${path}/${e.path}`;
                 buildFile(e, nextPath);
-                return {title: e.title, link: nextPath + '.json'};
+                return {title: e.title, link: 'https://api.learn.ryqn.dev' + nextPath + '.json'};
             });
             res(paths);
         });
@@ -30,7 +30,7 @@ const buildFolder = ( APIRoutes, path ) => {
 const buildFile = async ( content, path ) => {
     let staticAPIData = {...content};
     staticAPIData['content'] = await buildFolder(content, path);
-    fs.writeFile(path + '.json', JSON.stringify(staticAPIData), err => {
+    fs.writeFile('.' + path + '.json', JSON.stringify(staticAPIData), err => {
         if (err) throw err;
     });
    
